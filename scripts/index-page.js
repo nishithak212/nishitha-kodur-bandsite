13431ae5d9b18d1515fbefa5307461417a586295
nishithak212 <knishitha95@gmail.com>
2025-02-08 17:46:12 -0500

    Final commit for sprint-3

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,40 +3,47 @@ const api = new BandSiteApi("d0e75d6f-acc0-42fb-b5ef-3c6b0d4b2721");
 const commentsContainer=document.querySelector(".comments__list");
 const commentForm=document.querySelector("#comments__form");
 
-
-async function loadComments(){
+async function loadComments() {
     const comments = await api.getComments();
-    commentsContainer.innerHTML="";
+    commentsContainer.innerHTML = "";
     comments.forEach(displayComment);
 }
 
-function displayComment(comment){
-    const commentElement=document.createElement("div");
+function createCommentElement(comment) {
+    const commentElement = document.createElement("div");
     commentElement.classList.add("comments__default");
-    commentElement.innerHTML=`<div class="comments__dp-cont-one">
-      <div class="comments__header--dp-one"></div>
-    </div>
-    <div class="comments__header">
-      <h3 class="comments__header--name">${comment.name}</h3>
-      <h3 class="comments__header--date">${new Date(comment.timestamp).toLocaleDateString()}</h3>
-    </div>
-    <div class="comments__comments-cont-def">
-      <p class="comments__comments-cont-def--commentText">${comment.comment}</p>
-    </div>
+    commentElement.innerHTML = `
+        <div class="comments__dp-cont-one">
+            <div class="comments__header--dp-one"></div>
+        </div>
+        <div class="comments__header">
+            <h3 class="comments__header--name">${comment.name}</h3>
+            <h3 class="comments__header--date">${new Date(comment.timestamp).toLocaleDateString()}</h3>
+        </div>
+        <div class="comments__comments-cont-def">
+            <p class="comments__comments-cont-def--commentText">${comment.comment}</p>
+        </div>
     `;
+    return commentElement; 
+}
+
+
+function displayComment(comment) {
+    const commentElement = createCommentElement(comment); 
     commentsContainer.append(commentElement);
 }
 
-commentForm.addEventListener("submit",async (eventSubmit)=>{
+commentForm.addEventListener("submit", async (eventSubmit) => {
     eventSubmit.preventDefault();
-    const newComment ={
-        name:eventSubmit.target.name.value,
-        comment:eventSubmit.target.comment.value
+    const newComment = {
+        name: eventSubmit.target.name.value,
+        comment: eventSubmit.target.comment.value
     };
-
-    await api.postComment(newComment);
+    const postedComment = await api.postComment(newComment);
+    displayComment(postedComment);
     commentForm.reset();
     loadComments();
 });
+
 
 loadComments();
